ebd6f4230fedc19407bcb6bae8ec3649c1f65d61
xSqandal <sqandalx@gmail.com>
2020-07-25 17:01:13 +0300

    end project

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,28 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
-public class Main {
-    public static void main(String[] args) {
-        String folderPath = "C:/Users/incolor/Desktop/Java - по курсу";
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ParametersBag bag = new ParametersBag(args);
+
+        String folderPath = bag.getPath();
+        long sizeLimit = bag.getLimit();
+
         File file = new File(folderPath);
+        Node root = new Node(file, sizeLimit);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
 
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(root);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(calculator);
 
-    }
+        System.out.println(root);
 
-    public static long getFolderSize(File folder) {
-        if (folder.isFile()) {
-            return folder.length();
-        }
-
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for(File file : files) {
-            sum += getFolderSize(file);
-        }
-        return sum;
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 }
